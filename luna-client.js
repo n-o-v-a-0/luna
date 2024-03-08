4a758f0fa5a91a876af3aa15abb4234fa94739d6
n-o-v-a-0 <160797659+n-o-v-a-0@users.noreply.github.com>
2024-03-08 18:23:03 -0500

    quick UI fix

--- luna-client.js
+++ luna-client.js
@@ -36,8 +36,8 @@ function openPopup() {
   <div class="closeButton__34341" aria-label="Close" id="lunaClose"role="button" tabindex="0" style="float: right;"><svg aria-hidden="true" role="img" width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"></path></svg></div>
 <label for="lunaText"><img src="https://raw.githubusercontent.com/n-o-v-a-0/lunacdn/main/luna.png" style="float: left; max-width: 50px;"></label>
 <p class="display-lg__20b93" id="lunaText">Luna</p><br>
-<div class="flex"><p>Enter a theme URL:</p>
-<input class="inputDefault__80165 input_d266e7" placeholder="Your theme link" type="text" id="themeSelect"></div><br>
+<p>Enter a theme URL:</p>
+<input class="inputDefault__80165 input_d266e7" placeholder="Your theme link" type="text" id="themeSelect"><br><br>
 <div class="flex"><button type="submit" class="button_afdfd9 lookFilled__19298 colorBrand_b2253e sizeSmall__71a98 grow__4c8a4" id="saveTheme"><div class="contents_fb6220">Save & Reload</div></button><a href="https://betterdiscord.app/themes" target="_blank"><button aria-label="Edit display name" type="button" class="fieldButton_a93f59 button_afdfd9 lookFilled__19298 colorPrimary__6ed40 sizeSmall__71a98 grow__4c8a4"><div class="contents_fb6220">Themes</div></button></a></div><br>
 <div class="banner__79cec tier2__611e8 canceled__4a0b5"><div class="detailsContainer_d19ecf"><div class="details__695f1"><div class="planInfo_cd8cba">Luna will only work if you <a href="https://chromewebstore.google.com/detail/always-disable-content-se/ffelghdomoehpceihalcnbmnodohkibj" target="_blank">have this chrome extension.</a> If you already have it installed and enabled, you can safely ignore this.</div></div></div></div>
     `;
